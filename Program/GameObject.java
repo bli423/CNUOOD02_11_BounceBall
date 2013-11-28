9e1d4b6576f37651b1596095ac4ace1385c03a0c
heokyunam <heokyunam@naver.com>
2013-11-28 19:47:49 +0900

    Create GameObject.java

---
+++ Program/GameObject.java
@@ -0,0 +1,41 @@
+package Division;
+
+import java.awt.Graphics;
+/*
+ * GameObject는 abstract클래스이며,
+ * collisionCheck메소드와 draw메소드는 abstract메소드이다.
+ */
+public abstract class GameObject {
+	private int x;
+	private int y;
+	private int height;
+	private int width;
+	
+	public abstract void draw(Graphics g);
+	public abstract void collisionCheck(Ball ball);
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+}
